0ead785af946f937798da2470504ebb22c85e0d1
Zhiyang Wan <1293174374@qq.com>
2024-02-22 19:18:34 +0800

    调整事件调用，添加待补全代码

--- Assets/Scripts/Events/RecreateAllItemsEvent.cs
+++
@@ -1,6 +0,0 @@
-public class RecreateAllItemsEvent
-{
-    public RecreateAllItemsEvent()
-    {
-    }
-}
--- Assets/Scripts/Manager/TileMapGridManager.cs
+++ Assets/Scripts/Manager/TileMapGridManager.cs
@@ -1,13 +1,36 @@
+using System;
 using System.Collections.Generic;
+using CommonFramework.Runtime;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
-public class TileMapGridManager : MonoBehaviour
+public class TileMapGridManager : MonoSingleton<TileMapGridManager>
 {
     [SerializeField] private List<MapDataList> mapDataList;
+    [SerializeField] private RuleTile digTile;
+    [SerializeField] private RuleTile waterTile;
 
+    private Tilemap digTileMap;
+    private Tilemap waterTileMap;
     private Dictionary<string, Data.TileDetails> tileDetailsDict = new Dictionary<string, Data.TileDetails>();
+    private Grid currentGrid;
+
+    private void OnEnable()
+    {
+        EventCenter.StartListenToEvent<AfterSceneLoadEvent>(OnAfterSceneLoadEvent);
+    }
+
+    private void OnDisable()
+    {
+        EventCenter.StopListenToEvent<AfterSceneLoadEvent>(OnAfterSceneLoadEvent);
+    }
+
+    private void OnAfterSceneLoadEvent(AfterSceneLoadEvent obj)
+    {
+        currentGrid = FindObjectOfType<Grid>();
+        digTileMap = GameObject.FindWithTag("Dig").GetComponent<Tilemap>();
+        waterTileMap = GameObject.FindWithTag("Water").GetComponent<Tilemap>();
+    }
 
     private Data.TileDetails GetTileDetails(string key)
     {
@@ -18,7 +41,7 @@ public class TileMapGridManager : MonoBehaviour
 
         return null;
     }
-
+    
     private void InitTileDetailsDict(MapDataList mapDataList)
     {
         foreach (var tileData in mapDataList.TileDatas)
@@ -57,6 +80,49 @@ public class TileMapGridManager : MonoBehaviour
             {
                 tileDetailsDict.Add(key, tileDetails);
             }
+        }
+    }
+
+    /// <summary>
+    /// 待补充方法
+    /// </summary>
+    /// <param name="mouseWorldPos"></param>
+    /// <param name="itemDetails"></param>
+    private void OnExecuteAfterAnimation(Vector3 mouseWorldPos, Data.ItemDetails itemDetails)
+    {
+        Data.TileDetails currentTile = new Data.TileDetails();
+        switch (itemDetails.ItemType)
+        {
+            case Enums.ItemType.Commodity:
+                break;
+            case Enums.ItemType.HoeTool:
+                SetDigGround(currentTile);
+                currentTile.DaySinceDug = 0;
+                currentTile.CanDig = false;
+                currentTile.CanDrop = false;
+                break;
+            case Enums.ItemType.WaterTool:
+                SetWaterGround(currentTile);
+                currentTile.DaySinceWater = 0;
+                break;
+        }
+    }
+
+    private void SetDigGround(Data.TileDetails tileDetails)
+    {
+        Vector3Int pos = new Vector3Int(tileDetails.TileGridX, tileDetails.TileGridY, 0);
+        if (digTileMap != null)
+        {
+            digTileMap.SetTile(pos, digTile);
+        }
+    }
+
+    private void SetWaterGround(Data.TileDetails tileDetails)
+    {
+        Vector3Int pos = new Vector3Int(tileDetails.TileGridX, tileDetails.TileGridY, 0);
+        if (waterTileMap != null)
+        {
+            waterTileMap.SetTile(pos, waterTile);
         }
     }
 }
